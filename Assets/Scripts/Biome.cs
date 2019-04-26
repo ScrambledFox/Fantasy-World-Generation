6c75474e781abe2d49db8b0d420df804e563ecbe
Joris Lodewijks <info@jorislodewijks.nl>
2019-04-26 18:34:57 +0200

    Upload

---
+++ Assets/Scripts/Biome.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biome {
+    public enum Type {
+        Ocean, Sea, Forest, Desert, Plains, Tundra, Icecaps
+    }
+
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World {
+
+    string name;
+
+    ContinentType continentType;
+
+    float[,] heightMap;
+    Color[,] biomeMap;
+
+    public World (string name) {
+        this.name = name;
+    }
+
+    public enum ContinentType {
+        Pangea, Continents, Archipelago
+    }
+
+}
